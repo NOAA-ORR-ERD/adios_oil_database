15c4025035c5b1cf47233dc5ee4cd4107a6cb021
JamesMakela <james.makela@gmail.com>
2019-03-12 01:15:20 -0700

    pytests failing when importing models module.

--- oil_db/oil_database/models/__init__.py
+++ oil_db/oil_database/models/__init__.py
@@ -1,3 +1,6 @@
-from imported_rec import ImportedRecord
-from ec_imported_rec import ECImportedRecord
-from oil import Oil
+# Note: When we import Model classes into this namespace, PyMODM automatically
+#       tries to find a MongoDB connection and fails if there is none.
+#       This is messing up our pytests.
+# from imported_rec import ImportedRecord
+# from ec_imported_rec import ECImportedRecord
+# from oil import Oil
