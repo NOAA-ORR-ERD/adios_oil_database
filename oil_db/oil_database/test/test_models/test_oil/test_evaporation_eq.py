3fd65c7149412752fd699f9ddda8abd6c5107fba
James L. Makela <james.makela@noaa.gov>
2019-10-02 18:25:08 -0700

    The c coefficient in our evaporation eqs model is optional.  Fix the pytest.

--- oil_db/oil_database/test/test_models/test_oil/test_evaporation_eq.py
+++ oil_db/oil_database/test/test_models/test_oil/test_evaporation_eq.py
@@ -11,125 +11,86 @@ from oil_database.models.oil import EvaporationEq
 
 
 class TestEvaporationEq():
-    @pytest.mark.parametrize('a, b, c, equation',
+    @pytest.mark.parametrize('a, b, equation',
                              [
-                              (1.0, 2.0, 3.0, '(A + BT) ln t'),
-                              ('1.0', '2.0', '3.0', '(A + BT) ln t'),
+                              (1.0, 2.0, '(A + BT) ln t'),
+                              ('1.0', '2.0', '(A + BT) ln t'),
                               pytest.param(
-                                  None, None, None, None,
+                                  None, None, None,
                                   marks=pytest.mark.raises(exception=ValidationError)),
                               pytest.param(
-                                  None, None, None, '(A + BT) ln t',
+                                  None, None, '(A + BT) ln t',
                                   marks=pytest.mark.raises(exception=ValidationError)),
                               pytest.param(
-                                  None, None, 3.0, None,
+                                  None, 2.0, None,
                                   marks=pytest.mark.raises(exception=ValidationError)),
                               pytest.param(
-                                  None, None, 3.0, '(A + BT) ln t',
+                                  None, 2.0, '(A + BT) ln t',
                                   marks=pytest.mark.raises(exception=ValidationError)),
                               pytest.param(
-                                  None, 2.0, None, None,
+                                  1.0, None, None,
                                   marks=pytest.mark.raises(exception=ValidationError)),
                               pytest.param(
-                                  None, 2.0, None, '(A + BT) ln t',
+                                  1.0, None, '(A + BT) ln t',
                                   marks=pytest.mark.raises(exception=ValidationError)),
                               pytest.param(
-                                  None, 2.0, 3.0, None,
+                                  1.0, 2.0, None,
                                   marks=pytest.mark.raises(exception=ValidationError)),
                               pytest.param(
-                                  None, 2.0, 3.0, '(A + BT) ln t',
+                                  'nope', 2.0, '(A + BT) ln t',
                                   marks=pytest.mark.raises(exception=ValidationError)),
                               pytest.param(
-                                  1.0, None, None, None,
+                                  1.0, 'nope', '(A + BT) ln t',
                                   marks=pytest.mark.raises(exception=ValidationError)),
                               pytest.param(
-                                  1.0, None, None, '(A + BT) ln t',
-                                  marks=pytest.mark.raises(exception=ValidationError)),
-                              pytest.param(
-                                  1.0, None, 3.0, None,
-                                  marks=pytest.mark.raises(exception=ValidationError)),
-                              pytest.param(
-                                  1.0, None, 3.0, '(A + BT) ln t',
-                                  marks=pytest.mark.raises(exception=ValidationError)),
-                              pytest.param(
-                                  1.0, 2.0, None, None,
-                                  marks=pytest.mark.raises(exception=ValidationError)),
-                              pytest.param(
-                                  1.0, 2.0, None, '(A + BT) ln t',
-                                  marks=pytest.mark.raises(exception=ValidationError)),
-                              pytest.param(
-                                  1.0, 2.0, 3.0, None,
-                                  marks=pytest.mark.raises(exception=ValidationError)),
-                              pytest.param(
-                                  'nope', 2.0, 3.0, '(A + BT) ln t',
-                                  marks=pytest.mark.raises(exception=ValidationError)),
-                              pytest.param(
-                                  1.0, 'nope', 3.0, '(A + BT) ln t',
-                                  marks=pytest.mark.raises(exception=ValidationError)),
-                              pytest.param(
-                                  1.0, 2.0, 'nope', '(A + BT) ln t',
-                                  marks=pytest.mark.raises(exception=ValidationError)),
-                              pytest.param(
-                                  1.0, 2.0, 3.0, 'nope',
+                                  1.0, 2.0, 'nope',
                                   marks=pytest.mark.raises(exception=ValidationError)),
                               ])
-    def test_init_required(self, a, b, c, equation):
-        if a is None and b is None and c is None and equation is None:
+    def test_init_required(self, a, b, equation):
+        if a is None and b is None and equation is None:
             obj = EvaporationEq()
-        elif b is None and c is None and equation is None:
-            obj = EvaporationEq(a=a)
-        elif a is None and c is None and equation is None:
-            obj = EvaporationEq(b=b)
-        elif a is None and b is None and equation is None:
-            obj = EvaporationEq(c=c)
-        elif a is None and b is None and c is None:
-            obj = EvaporationEq(equation=equation)
-        elif c is None and equation is None:
-            obj = EvaporationEq(a=a, b=b)
         elif b is None and equation is None:
-            obj = EvaporationEq(a=a, c=c)
-        elif b is None and c is None:
-            obj = EvaporationEq(a=a, equation=equation)
+            obj = EvaporationEq(a=a)
         elif a is None and equation is None:
-            obj = EvaporationEq(b=b, c=c)
-        elif a is None and c is None:
-            obj = EvaporationEq(b=b, equation=equation)
+            obj = EvaporationEq(b=b)
         elif a is None and b is None:
-            obj = EvaporationEq(c=c, equation=equation)
+            obj = EvaporationEq(equation=equation)
         elif equation is None:
-            obj = EvaporationEq(a=a, b=b, c=c)
-        elif c is None:
-            obj = EvaporationEq(a=a, b=b, equation=equation)
+            obj = EvaporationEq(a=a, b=b)
         elif b is None:
-            obj = EvaporationEq(a=a, c=c, equation=equation)
+            obj = EvaporationEq(a=a, equation=equation)
         elif a is None:
-            obj = EvaporationEq(b=b, c=c, equation=equation)
+            obj = EvaporationEq(b=b, equation=equation)
         else:
-            obj = EvaporationEq(a=a, b=b, c=c, equation=equation)
+            obj = EvaporationEq(a=a, b=b, equation=equation)
 
         assert obj.a == float(a)
         assert obj.b == float(b)
-        assert obj.c == float(c)
         assert obj.equation == str(equation)
 
     def test_init_defaults(self):
-        obj = EvaporationEq(a=1.0, b=2.0, c=3.0, equation='(A + BT) ln t')
+        obj = EvaporationEq(a=1.0, b=2.0, equation='(A + BT) ln t')
 
         assert obj.weathering == 0.0
+        assert obj.c is None
 
-    @pytest.mark.parametrize('weathering',
+    @pytest.mark.parametrize('c, weathering',
                              [
-                              0.1,
-                              '0.1',
+                              (1.0, 0.1),
+                              ('1.0', '0.1'),
                               pytest.param(
-                                  'nope',
+                                  'nope', 0.1,
+                                  marks=pytest.mark.raises(exception=ValidationError)),
+                              pytest.param(
+                                  1.0, 'nope',
                                   marks=pytest.mark.raises(exception=ValidationError)),
                               ])
-    def test_init_optional(self, weathering):
-        obj = EvaporationEq(a=1.0, b=2.0, c=3.0, equation='(A + BT) ln t',
+    def test_init_optional(self, c, weathering):
+        obj = EvaporationEq(a=1.0, b=2.0, c=c, equation='(A + BT) ln t',
                             weathering=weathering)
 
         assert obj.weathering == float(weathering)
+        assert obj.c == float(c)
 
     @pytest.mark.parametrize('equation, t, T, expected',
                              [
