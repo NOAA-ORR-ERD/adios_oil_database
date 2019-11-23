95cbccf397b767d33cf9bcb9e75a349aa2d91505
James L. Makela <james.makela@noaa.gov>
2019-11-23 12:56:04 -0800

    Created a method to configure the oil adapter host via an externally bundled json config file

--- web_client/app/adapters/application.js
+++ web_client/app/adapters/application.js
@@ -1,5 +1,4 @@
 import RESTAdapter from 'ember-data/adapters/rest';
 
 export default RESTAdapter.extend({
-  host: 'http://localhost:9898',
 });
--- web_client/app/routes/oils/index.js
+++ web_client/app/routes/oils/index.js
@@ -2,6 +2,15 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return this.store.peekAll('oil');
+      return (async () => {
+          let config = await this.store.findRecord('config', 'main.json');
+          var adapter = await this.store.adapterFor('oil');
+
+          if (adapter.host !== config.webApi) {
+            adapter = await adapter.reopen({host: config.webApi});
+          }
+
+          return this.store.peekAll('oil');
+        })();
   }
 });
