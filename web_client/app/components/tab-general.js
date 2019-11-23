5bc842e245810696878f8ffdb84121b6972507b5
Gennady Kachook <Gennady.Kachook@noaa.gov>
2019-11-22 17:52:56 -0800

    Added basic action for API editing

--- web_client/app/components/tab-general.js
+++ web_client/app/components/tab-general.js
@@ -1,4 +1,9 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    actions: {
+        updateAPI() {
+            alert('API value has been changed!');
+        }
+    }
 });
