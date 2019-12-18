af04e18a8f93537f4ca1b449550e9189d76ff442
Chris Barker <Chris.Barker@noaa.gov>
2019-12-18 14:12:40 -0800

    added file for "canonical" oil

---
+++ oil_db/oil_database/models/oil/canonical.py
@@ -0,0 +1,18 @@
+
+def get_new_oil(name, sparse=False):
+    """
+    returns a new, complete empty oil record, with the name set
+    """
+    if sparse:
+        raise NotImplementedError
+    elif:
+        oil = copy.deepcopy(EMPTY_RECORD)
+        oil["name"] = name
+
+    return oil
+
+
+RECORD_SPEC = {"_id": "XX000000"
+               "name": "dummy name",
+# everything else here
+               }
