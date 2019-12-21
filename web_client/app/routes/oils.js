6845ebb4161e075f78c1820ec0554dc99aa095ef
James L. Makela <james.makela@noaa.gov>
2019-12-20 17:20:01 -0800

    Both oil & category models are properly initialized

--- web_client/app/routes/oils.js
+++ web_client/app/routes/oils.js
@@ -1,16 +1,36 @@
 import Route from '@ember/routing/route';
+import { hash } from 'rsvp';
 
 export default Route.extend({
-    model(params) {
-        return (async () => {
-            let config = await this.store.findRecord('config', 'main.json');
-            var adapter = await this.store.adapterFor('oil');
+    model() {
 
-            if (adapter.host !== config.webApi) {
-              adapter = await adapter.reopen({host: config.webApi});
-            }
+        return hash({
+            oils: (async () => {
+                let config = await this.store.findRecord('config', 'main.json');
+                var adapter = await this.store.adapterFor('oil');
 
-            return this.store.findAll('oil');
-          })();
+                if (adapter.host !== config.webApi) {
+                  adapter = await adapter.reopen({host: config.webApi});
+                }
+
+                return this.store.findAll('oil');
+            })(),
+
+            categories: (async () => {
+                let config = await this.store.findRecord('config', 'main.json');
+                var adapter = await this.store.adapterFor('category');
+
+                if (adapter.host !== config.webApi) {
+                  adapter = await adapter.reopen({host: config.webApi});
+                }
+
+                return this.store.findAll('category');
+            })(),
+        });
+    },
+
+    setupController(controller, models) {
+        controller.setProperties(models);
     }
+
 });
