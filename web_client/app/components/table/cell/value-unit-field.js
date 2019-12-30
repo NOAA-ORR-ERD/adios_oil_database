fae34ee58ca1a3b711217d60e1539fabd8c37725
Gennady Kachook <Gennady.Kachook@noaa.gov>
2019-12-29 19:51:38 -0800

    Added value-unit-field component Added editable mode for Pour Point

---
+++ web_client/app/components/table/cell/value-unit-field.js
@@ -0,0 +1,18 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+
+    init(){
+        this._super(...arguments);
+
+        {{debugger}}
+        
+    },
+
+    actions: {
+        updateValue() {
+
+        }
+    }
+
+});
