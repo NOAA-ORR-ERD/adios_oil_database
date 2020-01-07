744ff70d0a712a71db2e933d3d17d0f00dbc641a
James L. Makela <james.makela@noaa.gov>
2020-01-07 11:16:48 -0800

    better interaction between the add button and the add dialog

--- web_client/app/components/tab-nav/add-sample-dlg.js
+++ web_client/app/components/tab-nav/add-sample-dlg.js
@@ -1,4 +1,38 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from "@ember/object";
 
-export default Component.extend({
-});
+export default class AddSampleDlg extends Component {
+    @tracked sampleType = 0;
+
+    SampleTypes = {
+            weathered: 0,
+            distillate: 1,
+            nameOnly: 2
+    };
+
+    @action
+    updateSampleType(choice) {
+        console.log('changing subsample type choice',
+                    'from', this.sampleType,
+                    'to', choice.target.value);
+
+        this.sampleType = Number(choice.target.value);
+    }
+
+    @action
+    addSample() {
+        console.log('Adding sample...');
+
+        let oil = this.get('oil');
+        let samples = oil.get('samples');
+
+        samples.pushObject({
+            name: 'new sample',
+            short_name: 'new sample'
+        });
+
+        this.submit(oil);
+    }
+
+};
