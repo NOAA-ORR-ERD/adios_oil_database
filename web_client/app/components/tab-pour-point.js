93fd3182f6c27d5e878d21569d4b4f419cacbcef
Gennady Kachook <Gennady.Kachook@noaa.gov>
2020-01-08 12:28:21 -0800

    Added Save/Cancel actions to scalar-range dialog

--- web_client/app/components/tab-pour-point.js
+++ web_client/app/components/tab-pour-point.js
@@ -12,19 +12,20 @@ export default Component.extend({
     scalarValue: undefined,
 
     init(){
+        {{debugger}}
         this._super(...arguments);
         let oil = this.get('oil');
         if(oil.pour_point){
-            if(oil.pour_point.value){
-                this.scalarValue = oil.pour_point.value;
+            if(oil.pour_point.ref_temp.value){
+                this.scalarValue = oil.pour_point.ref_temp.value;
                 this.isInterval = false;
             }
-            if(oil.pour_point.min_value){
-                this.scalarValue = oil.pour_point.min_value;
+            if(oil.pour_point.ref_temp.min_value){
+                this.scalarValue = oil.pour_point.ref_temp.min_value;
                 this.isInterval = true;
             }
-            if(oil.pour_point.max_value){
-                this.scalarValue = oil.pour_point.max_value;
+            if(oil.pour_point.ref_temp.max_value){
+                this.scalarValue = oil.pour_point.ref_temp.max_value;
                 this.isInterval = true;
             }
         }
@@ -43,21 +44,29 @@ export default Component.extend({
 
     actions: {
 
-        toggleModal(){
-            this.toggleProperty('isShowingModal');
-            if(this.isShowingModal)
-            {
-                this.toggleInput();
-            }
+        openModalDialog(){
+            //$("#scalar-range-dialog").modal();
+            this.set('isShowingModal', true);
+            this.toggleInput();
+        },
+
+        closeModalDialog() {
+            this.set('isShowingModal', false);
         },
 
         toggleRadio(){
             this.toggleProperty('isInterval');
             this.toggleInput();
         },
+    
+        onSave(){
+            alert("Saving");
+            //$("#scalar-range-dialog").hide();
+        },
 
-        getModalInput(){
-            alert("!!!");
+        onCancel(){
+            alert("Closing");
+            this.CloseModalDialog();
         }
     }
 });
