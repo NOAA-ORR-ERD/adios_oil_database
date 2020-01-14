2ee395213ef8fb041e3ad7b387291b557a5af561
Gennady Kachook <Gennady.Kachook@noaa.gov>
2020-01-13 17:01:19 -0800

    Fixed adhesion update

--- web_client/app/components/table/adhesion.js
+++ web_client/app/components/table/adhesion.js
@@ -1,15 +1,20 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { action } from "@ember/object";
 
 export default class Adhesion extends Component {
 
+    @tracked adhesion;
+
     constructor() {
         super(...arguments);
+
+        this.adhesion = this.args.oil.adhesion;
     }
 
     @action
     updateAdhesion() {
-        // TODO
+        this.args.oil.adhesion = this.adhesion;
     }
 
 }
