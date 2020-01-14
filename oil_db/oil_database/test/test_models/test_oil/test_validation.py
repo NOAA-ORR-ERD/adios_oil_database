1ccb5cd68d332b4e6ecd598836d08e6b509c424c
Chris Barker <Chris.Barker@noaa.gov>
2020-01-14 07:14:13 -0800

    added new validator -- only one so far.

--- oil_db/oil_database/test/test_models/test_oil/test_validation.py
+++ oil_db/oil_database/test/test_models/test_oil/test_validation.py
@@ -13,8 +13,25 @@ def no_type_oil():
     return no_type_oil
 
 
-
-def test_type(no_type_oil):
+def test_no_type(no_type_oil):
     validate(no_type_oil)
 
-    assert no_type_oil["status"] == []
+    assert 'W001: Record has no product type' in no_type_oil["status"]
+
+
+def test_bad_type(no_type_oil):
+    no_type_oil['product_type'] = "Fred"
+    validate(no_type_oil)
+
+    print(no_type_oil["status"])
+    assert "W002: Record has no product type. Options are: ('crude', 'refined', 'bitumen product', 'other')" in no_type_oil["status"]
+
+
+def test_correct_type(no_type_oil):
+    no_type_oil['product_type'] = "Crude"
+    validate(no_type_oil)
+
+    print(no_type_oil["status"])
+    for msg in no_type_oil["status"]:
+        assert not msg.startswith("W001")
+        assert not msg.startswith("W002")
