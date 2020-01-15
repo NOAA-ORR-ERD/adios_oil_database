8f03bae446f313726d2ca56e21f14d19ec363e09
Gennady Kachook <Gennady.Kachook@noaa.gov>
2020-01-14 16:54:20 -0800

    Added unit output in edit mode

--- web_client/app/components/common/value-unit-input.js
+++ web_client/app/components/common/value-unit-input.js
@@ -1,10 +1,14 @@
 import Component from '@glimmer/component';
 import { action } from "@ember/object";
+import { valueUnitUnit } from 'ember-oil-db/helpers/value-unit-unit';
 
 export default class ValueUnitInput extends Component {
 
     constructor() {
         super(...arguments);
+
+        let unitObj = {"unit": this.args.valueUnit};
+        this.beaUnit = valueUnitUnit([unitObj]);
     }
 
     @action
