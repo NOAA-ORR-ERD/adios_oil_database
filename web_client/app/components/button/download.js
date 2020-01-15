40e9840dbc6526f95bab7bfd6746d45974f54ebf
James L. Makela <james.makela@noaa.gov>
2020-01-14 21:35:17 -0800

    Slugify the filename to download

--- web_client/app/components/button/download.js
+++ web_client/app/components/button/download.js
@@ -1,14 +1,17 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { action } from "@ember/object";
+import slugify from 'ember-slugify';
 
 export default class Download extends Component {
     @service download;
 
     @action
     downloadOil() {
+        let fileName = slugify(this.args.oil.name);
+
         this.download.asJSON(
-            `${this.args.oil.name}.json`,
+            `${fileName}.json`,
             JSON.stringify(this.args.oil.serialize(), null, 2)
         );
     }
