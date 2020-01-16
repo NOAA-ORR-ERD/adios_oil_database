864d1c144e62dc9bccfc3e04af023ce58b6cd654
James L. Makela <james.makela@noaa.gov>
2020-01-16 11:50:12 -0800

    Added capability to upload an oil from a local .json file

---
+++ web_client/app/components/dialog/upload.js
@@ -0,0 +1,50 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from "@ember/object";
+
+export default class UploadDlg extends Component {
+    @tracked file;
+
+    get formFilledOut() {
+        if (this.file) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @action uploadedFile(data) {
+        let oilData = JSON.parse(data);
+        delete oilData['id'];
+
+        this.args.submit(oilData);
+    }
+
+    @action
+    updateFile(event) {
+        if (event.target.files.length) {
+            this.file = event.target.files[0];
+        }
+        else {
+            this.file = null;
+        }
+
+    }
+
+    @action
+    submitForm() {
+        const reader = new FileReader();
+        const file = this.file;
+
+        // Note: reading file is async
+        reader.onload = () => {
+            this.uploadedFile(reader.result);
+        };
+
+        if (file) {
+            reader.readAsText(file);
+        }
+
+    }
+
+}
