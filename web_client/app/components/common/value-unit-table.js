68ee09857f4d19e4c8e7c932fd827a29c5bf879e
Gennady Kachook <Gennady.Kachook@noaa.gov>
2020-01-16 17:39:03 -0800

    Added delete/add table line controls

--- web_client/app/components/common/value-unit-table.js
+++ web_client/app/components/common/value-unit-table.js
@@ -1,4 +1,22 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from "@ember/object";
 
-export default Component.extend({
-});
+export default class ValueUnitTable extends Component {
+
+    @tracked valueArray = [];
+
+    constructor() {
+        super(...arguments);
+
+        this.tableId = this.args.tableTitle.replace(/\s+/g, '-').toLowerCase();
+
+        this.valueArray = this.args.valueArray;
+    }
+
+    @action
+    updateValue() {
+        // TODO
+    }
+    
+}
