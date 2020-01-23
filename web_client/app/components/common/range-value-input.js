46586811db14a1634ad82d34a6fac6d6973893bb
James L. Makela <james.makela@noaa.gov>
2020-01-23 05:22:46 -0800

    Sample flash point & pour point can be saved to the database

--- web_client/app/components/common/range-value-input.js
+++ web_client/app/components/common/range-value-input.js
@@ -1,6 +1,7 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import { action } from "@ember/object";
+import { action, set } from "@ember/object";
+
 
 export default class RangeValueInput extends Component {
 
@@ -14,13 +15,74 @@ export default class RangeValueInput extends Component {
         this.valueObject = this.args.valueObject;
 
         if(this.valueObject){
-            this.inputValue = this.valueObject[this.args.valueName];
+            this.inputValue = this.deepGet(this.valueObject, this.args.valueName);
         }
         // form component ID - it needs to tether ember-modal-dialog to - replace spaces too
         this.componentId = this.args.valueTitle.replace(/\s+/g, '-').toLowerCase() + 
             this.args.sampleIndex;
     }
+
+    deepGet(obj, path) {
+        for (var i = 0, attrs = path.split('.'), len = attrs.length;
+        i < len; i++) {
+            try {
+                obj = obj[attrs[i]];
+            }
+            catch(err) {
+                return obj;  // undefined
+            }
+        }
+
+        return obj;
+    }
+
+    deepSet(obj, path, value) {
+        // protect against being something unexpected
+        obj = typeof obj === 'object' ? obj : {};
+
+        // split the path into and array if its not one already
+        var keys = Array.isArray(path) ? path : path.split('.');
+        var key;
+
+        // loop over the path parts one at a time
+        // but, dont iterate the last part,
+        for (var i = 0; i < keys.length - 1; i++) {
+            key = keys[i];
+
+            if (!obj[key] && !Object.prototype.hasOwnProperty.call(obj, key)) {
+                // if nothing exists for this key, make it an empty object or array
+                // get the next key in the path, if its numeric, make this property
+                // an empty array.  Otherwise, make it an empty object
+                var nextKey = keys[i+1];
+                var useArray = /^\+?(0|[1-9]\d*)$/.test(nextKey);
+                obj[key] = useArray ? [] : {};
+            }
+
+            obj = obj[key];
+        }
+
+        // set our value
+        // we need to use @ember/object:set() here because our oil is a tracked
+        // property, and it is defined as old-style ember, not octane.
+        key = keys[i];
+        set(obj, key, value);
+
+        return obj;
+    }
     
+    enteredValuesValid(entered) {
+        if (!entered) { return false }
+
+        if (!(entered.hasOwnProperty("min_value") ||
+              entered.hasOwnProperty("max_value") ||
+              entered.hasOwnProperty("value")))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
     get editable() {
         return this.args.editable;
     }
@@ -37,83 +99,28 @@ export default class RangeValueInput extends Component {
 
     @action
     updateValue(enteredValue) {
-        // TODO - do we need to keep properties other than value(s) and unit? (_cls, from_unit, etc.)
-
-        if (enteredValue) {
-            // check if all entered fields are not empty
-            if (enteredValue.hasOwnProperty("min_value") ||
-                enteredValue.hasOwnProperty("max_value") ||
-                enteredValue.hasOwnProperty("value")) {
-
-                if (this.valueObject) {
-                    // there are source and enterd value(s) case
-                    // min value
-                    if(enteredValue.hasOwnProperty("min_value")){
-                        this.valueObject[this.args.valueName].min_value = enteredValue.min_value;
-                    } else {
-                        if(this.valueObject[this.args.valueName].hasOwnProperty("min_value")) {
-                            delete this.valueObject[this.args.valueName].min_value;
-                        }
-                    }
-                    // max value
-                    if (enteredValue.hasOwnProperty("max_value")) {
-                        this.valueObject[this.args.valueName].max_value = enteredValue.max_value;
-                    } else {
-                        if(this.valueObject[this.args.valueName].hasOwnProperty("max_value")) {
-                            delete this.valueObject[this.args.valueName].max_value
-                        }
-                    }
-                    // scalar value
-                    if (enteredValue.hasOwnProperty("value")) {
-                        this.valueObject[this.args.valueName].value = enteredValue.value;
-                    } else {
-                        if(this.valueObject[this.args.valueName].hasOwnProperty("value")) {
-                            delete this.valueObject[this.args.valueName].value
-                        }
-                    }
-                    // save unit
-                    this.valueObject[this.args.valueName].unit = enteredValue.unit;
-
-                } else {
-                    // there is enterd value but there is no source object - create one
-                    this.valueObject = {
-                        [this.args.valueName]: {
-                            unit: enteredValue.unit
-                        }
-                    };
-
-                    if (enteredValue.hasOwnProperty("value")) {
-                        this.valueObject[this.args.valueName].value = enteredValue.value;
-                    } else {
-                        if (enteredValue.hasOwnProperty("min_value")) {
-                            this.valueObject[this.args.valueName].min_value = enteredValue.min_value;
-                        } 
-                        if (enteredValue.hasOwnProperty("max_value")) {
-                            this.valueObject[this.args.valueName].max_value = enteredValue.max_value;
-                        } 
-                    }
-                }
-            } else {
-                // all entered fields are empty - remove source
-                if (this.valueObject) {
-                    this.valueObject = null;
-                }
-            }
-        } else {
-            // nothing entered - remove source 
-            if (this.valueObject) {
-                this.valueObject = null;
-            }
+        let newValue;
+        if (this.enteredValuesValid(enteredValue)) {
+            newValue = enteredValue;
+        }
+        else {
+            // nothing entered - This happens when we have no values in the
+            // dialog and then press OK.  We will view this as an intentional
+            // nullification of the property.
+            newValue = null;
         }    
 
-        if (this.valueObject && this.valueObject[this.args.valueName]) {
-            this.inputValue = this.valueObject[this.args.valueName];
-        } else {
-            this.inputValue = null;
-        }
+        this.deepSet(this.valueObject, this.args.valueName, newValue);
+        this.inputValue = newValue;
+
+        console.log('setting this.valueObject...');
         this.valueObject = this.valueObject; // ember! - reset tracked object
 
-        // TODO
-        //this.args.updateValue(valueObject);
+        if (this.args.submit) {
+            this.args.submit();
+        }
+        else {
+            console.warn('No submit method for', this.args.valueName);
+        }
     }
 }
--- web_client/app/components/tab-flash-point.js
+++ web_client/app/components/tab-flash-point.js
@@ -1,5 +1,10 @@
 import Component from '@glimmer/component';
+import { action } from "@ember/object";
 
 export default class TabFlashPoint extends Component {
     
+    @action
+    submit() {
+        this.args.submit(this.args.oil);
+    }
 }
