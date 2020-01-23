cfdf57c840897c241619830bb4e0a030a500a417
James L. Makela <james.makela@noaa.gov>
2020-01-23 12:53:32 -0800

    Sample adhesion can be saved to the database

--- web_client/app/components/table/adhesion.js
+++ web_client/app/components/table/adhesion.js
@@ -3,32 +3,8 @@ import { tracked } from '@glimmer/tracking';
 import { action } from "@ember/object";
 
 export default class Adhesion extends Component {
-
-    @tracked adhesion;
-
-    constructor() {
-        super(...arguments);
-        this.adhesion = this.args.oil.adhesion;
-    }
-
     @action
-    updateAdhesion(e) {
-
-        // if (isNaN(e.target.value)) {
-        //     if (this.adhesion) {
-        //         this.adhesion = undefined;
-        //     }
-        // } else {
-        //     if (this.adhesion) {
-        //         this.adhesion.adhesion.value = Number(e.target.value);
-        //     } else {
-        //         let adhesion = {};
-        //         adhesion.value = Number(e.target.value);
-        //         adhesion.unit = "N/m^2";
-        //         //this.adhesion.pushObject(adhesion);
-        //     }
-        // }
-        // TODO - No Data case
-        //this.args.submit(this.args.oil);
+    submit() {
+        this.args.submit(this.args.oil);
     }
 }
