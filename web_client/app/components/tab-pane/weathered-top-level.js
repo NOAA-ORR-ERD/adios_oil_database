b6a018d008ba4fcb71a2f4659854c94a27bd6bbe
James L. Makela <james.makela@noaa.gov>
2020-01-28 11:51:42 -0800

    The last category tab viewed is persisted between route transitions. This is important, because tab component state gets reinitialized while editing.

--- web_client/app/components/tab-pane/weathered-top-level.js
+++ web_client/app/components/tab-pane/weathered-top-level.js
@@ -19,7 +19,49 @@ export default Component.extend({
         this.set('sampleIndex', sampleIndex);
     },
 
+    didInsertElement() {
+        if (this.savedCategoryTab &&
+            this.element.querySelector(this.savedCategoryTab))
+        {
+            this.element.querySelectorAll('.tab-pane').forEach(i => {
+                i.classList.remove('active', 'show');
+            });
+            this.element.querySelectorAll('a[data-toggle="tab"]').forEach(i => {
+                i.classList.remove('active');
+                i.setAttribute('aria-selected', false);
+            });
+
+            let elem = this.element.querySelector(`a[href="${this.savedCategoryTab}"]`);
+            elem.setAttribute('aria-selected', true);
+            elem.classList.add('active');
+
+            this.element.querySelector(this.savedCategoryTab)
+                .classList.add('active', 'show');
+        }
+        else {
+            // just use the default behavior
+            this.element.querySelectorAll('.tab-pane').forEach(i => {
+                if (!i.classList.contains('show')) {
+                    console.log(i.classList.remove('active'));
+                }
+            });
+        }
+
+        // Note: Ember doesn't want you to use JQuery for some purity reason,
+        //       and it throws warnings when the app starts.
+        //       Unfortunately, JQuery is the only way to add an event listener
+        //       to a bootstrap tab.
+        $(this.element).find('a[data-toggle="tab"]')
+                       .off("shown.bs.tab")
+                       .on('shown.bs.tab', this, this.actions.shown);
+
+    },
+
     actions: {
+        shown(event) {
+            event.data.set('savedCategoryTab', event.currentTarget.hash);
+        },
+
         submitSample(sample) {
             let oil = this.get('oil');
             // let samples =  oil.samples;
---
+++ web_client/app/controllers/oils/show.js
@@ -0,0 +1,6 @@
+import Controller from '@ember/controller';
+import { action } from "@ember/object";
+
+export default class ShowController extends Controller {
+    savedCategoryTab = '#nav-physical-fresh-oil-sample'
+}
