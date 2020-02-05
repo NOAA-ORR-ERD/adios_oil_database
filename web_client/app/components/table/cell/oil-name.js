6131cfa368036b1e5d401bcc55a7da2f8b0e47ae
James L. Makela <james.makela@noaa.gov>
2020-02-04 18:08:39 -0800

    Converted query table cell components to glimmer

--- web_client/app/components/table/cell/oil-name.js
+++ web_client/app/components/table/cell/oil-name.js
@@ -1,10 +1,21 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
-export default Component.extend({
-  init() {
-    this._super(...arguments);
+export default class OilName extends Component {
+    @tracked oilId;
 
-    this.set('oilId', arguments[0].attrs.row.value.content.id);
-  },
+    constructor() {
+        super(...arguments);
 
-});
+        this.oilId = this.args.row.content.id;
+    }
+}
+
+//export default Component.extend({
+//  init() {
+//    this._super(...arguments);
+//
+//    this.set('oilId', arguments[0].attrs.row.value.content.id);
+//  },
+//
+//});
