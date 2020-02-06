f1d800b7fb37fe83d9a07c49721feb51536bdc41
James L. Makela <james.makela@noaa.gov>
2020-02-06 11:45:15 -0800

    Converted oil demographics pane component to glimmer

--- web_client/app/components/oil-demographics.js
+++ web_client/app/components/oil-demographics.js
@@ -1,104 +1,94 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from "@ember/object";
 import moment from 'moment';
 import { isBlank , isNone} from '@ember/utils';
 
-export default Component.extend({
+export default class Composition extends Component {
+    @tracked oilCategories = undefined;
+    @tracked selectedCategories = undefined;
 
-    oilCategories: undefined,
-    selectedCategories: undefined,
+    constructor() {
+        super(...arguments);
 
-    init(){
-        this._super(...arguments);
-        this.set('oilCategories', this.getOilCategories());
-        this.set('selectedCategories', this.oil.categories);
-    },
+        this.oilCategories = this.getOilCategories();
+        this.selectedCategories = this.args.oil.categories;
+    }
 
     getOilCategories() {
-        return this.oil.get('store').findAll('category')
-            .then(function(response) {
-                return response.toArray().map(i => {return i.name});
-            });
-    },
-  
-    actions: {
-        updateAPI(event) {
-            {{debugger}}
-            let oil = this.get('oil');
-            let enteredAPI = event.target.value;
-            if (isBlank(enteredAPI)) {
-                delete oil.samples.get(0).apis;
-                // in case there is api on top level
-                if (!isNone(oil.api)) {
-                    delete oil.api;
-                }
-            } else {
-                if (isNone(oil.samples.get(0).apis)) {
-                    oil.samples.get(0).apis = [{gravity: Number(enteredAPI)}];
-                } else {
-                    oil.samples.get(0).apis.set('0.gravity',  Number(enteredAPI));
-                }
-                // for the new api place
-                if (isNone(oil.api)) {
-                    oil.api = Number(enteredAPI);
-                } else {
-                    oil.set('api',  Number(enteredAPI));
-                }
+        return this.args.oil.store.findAll('category').then(function(response) {
+            return response.toArray().map(i => {return i.name});
+        });
+    }
 
+    @action
+    updateAPI(event) {
+        let enteredAPI = event.target.value;
+
+        if (isBlank(enteredAPI)) {
+            delete this.args.oil.samples.get(0).apis;
+            // in case there is api on top level
+            if (!isNone(this.args.oil.api)) {
+                delete this.args.oil.api;
+            }
+        }
+        else {
+            if (isNone(this.args.oil.samples.get(0).apis)) {
+                this.args.oil.samples.get(0).apis = [{gravity: Number(enteredAPI)}];
+            }
+            else {
+                this.args.oil.samples.get(0).apis.set('0.gravity',  Number(enteredAPI));
             }
 
-            this.submit(oil);
-        },
-
-        updateLocation(event) {
-            let oil = this.get('oil');
-            oil.set('location', event.target.value);
-
-            this.submit(oil);
-        },
-
-        updateType(event) {
-            let oil = this.get('oil');
-            oil.set('productType', event.target.value);
-
-            this.submit(oil);
-        },
-
-        updateCategories(selectedCategories) {
-            this.set('selectedCategories', selectedCategories);  
-
-            let oil = this.get('oil');
-            oil.set('categories', selectedCategories);
-
-            this.submit(oil);
-        },
-
-        updateReference(event) {
-            let oil = this.get('oil');
-            oil.set('reference', event.target.value);
-
-            this.submit(oil);
-        },
-
-        updateReferenceDate(event) {
-            let oil = this.get('oil');
-            oil.set('referenceDate', moment(event.target.value, "YYYY-MM-DD").tz("Europe/London").unix());
-
-            this.submit(oil);
-        },
-
-        updateSampleReceivedDate(event) {
-            let oil = this.get('oil');
-            oil.set('sampleDate', moment(event.target.value, "YYYY-MM-DD").tz("Europe/London").unix());
-
-            this.submit(oil);
-        },
-
-        updateComments(event) {
-            let oil = this.get('oil');
-            oil.set('comments', event.target.value);
-
-            this.submit(oil);
+            this.args.oil.api = Number(enteredAPI);
         }
-        
+
+        this.args.submit(this.args.oil);
     }
-});
+
+    @action
+    updateLocation(event) {
+        this.args.oil.location = event.target.value;
+        this.args.submit(this.args.oil);
+    }
+
+    @action
+    updateType(event) {
+        this.args.oil.productType = event.target.value;
+        this.args.submit(this.args.oil);
+    }
+
+    @action
+    updateCategories(selectedCategories) {
+        this.selectedCategories = selectedCategories;
+        this.args.oil.categories = selectedCategories;
+        this.args.submit(this.args.oil);
+    }
+
+    @action
+    updateReference(event) {
+        this.args.oil.reference = event.target.value;
+        this.args.submit(this.args.oil);
+    }
+
+    @action
+    updateReferenceDate(event) {
+        this.args.oil.referenceDate = moment(event.target.value, "YYYY-MM-DD")
+            .tz("Europe/London").unix();
+        this.args.submit(this.args.oil);
+    }
+
+    @action
+    updateSampleReceivedDate(event) {
+        this.args.oil.sampleDate = moment(event.target.value, "YYYY-MM-DD")
+            .tz("Europe/London").unix();
+        this.args.submit(this.args.oil);
+    }
+
+    @action
+    updateComments(event) {
+        this.args.oil.comments = event.target.value;
+        this.args.submit(this.args.oil);
+    }
+
+}
--- web_client/app/components/tab-pane/physical.js
+++ web_client/app/components/tab-pane/physical.js
@@ -1,4 +1,4 @@
 import Component from '@glimmer/component';
 
-export default class Composition extends Component {
+export default class Physical extends Component {
 }
