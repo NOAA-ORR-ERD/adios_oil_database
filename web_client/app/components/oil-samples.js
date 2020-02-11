37551e47fe6a951e5aa0d1c901ee418cb850d404
James L. Makela <james.makela@noaa.gov>
2020-02-11 07:55:07 -0800

    Converted the oil subsamples tab navigation component to glimmer

--- web_client/app/components/oil-samples.js
+++ web_client/app/components/oil-samples.js
@@ -1,62 +1,98 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from "@ember/object";
+import slugify from 'ember-slugify';
 import $ from 'jquery';
 
-export default Component.extend({
+export default class SubSample extends Component {
 
-    didRender() {
-        this._super(...arguments);
-
-        // clear the slate
-        this.element.querySelectorAll('a[data-toggle="tab"][tab-level="0"]')
-        .forEach(i => {
-            i.classList.remove('active');
-            i.setAttribute('aria-selected', false);
-        });
-
-        this.element.querySelectorAll('.tab-pane[tab-level="0"]')
-        .forEach(i => {
-            i.classList.remove('active', 'show');
-        });
-
-
-        if (this.sampleTab &&
-            this.element.querySelector(this.sampleTab))
-        {
-            let elem = this.element.querySelector(`a[href="${this.sampleTab}"]`);
-            elem.setAttribute('aria-selected', true);
-            elem.classList.add('active');
-
-            this.element.querySelector(this.sampleTab)
-                .classList.add('active', 'show');
+    sampleTab() {
+        if (this.args.sampleTab) {
+            // get the last active subsample tab
+            return this.args.sampleTab.slice('#nav-'.length);
         }
         else {
-            // set the default tab to the first one
-            let elem = this.element.querySelector('a[data-toggle="tab"][tab-level="0"]')
-            elem.setAttribute('aria-selected', true);
-            elem.classList.add('active');
-
-            this.element.querySelector('.tab-pane[tab-level="0"]')
-                .classList.add('active', 'show');
+            // just choose the first tab
+            return slugify(this.args.oil.samples[0].name);
         }
+    }
 
+    get navTabProperties() {
+        let savedTab = this.sampleTab();
+
+        return this.args.oil.samples.map(s => {
+            let tabName = slugify(s.name);
+            let ret = {
+                'name': s.name,
+                'short_name': s.short_name,
+                'id': 'nav-' + tabName + '-tab',
+                'href': '#nav-' + tabName,
+                'aria-controls': 'nav-' + tabName
+            };
+
+            if (savedTab === tabName) {
+                return {
+                    ...ret,
+                    'class': 'nav-item nav-link active',
+                    'aria-selected': true
+                };
+            }
+            else {
+                return {
+                    ...ret,
+                    'class': 'nav-item nav-link',
+                    'aria-selected': false
+                };
+            }
+        });
+    }
+
+    get tabPaneProperties() {
+        let sampleTab = this.sampleTab();
+
+        return this.args.oil.samples.map(s => {
+            let tabName = slugify(s.name);
+            let ret = {
+                'name': s.name,
+                'short_name': s.short_name,
+                'id': 'nav-' + tabName,
+                'aria-labelledby': 'nav-' + tabName + '-tab'
+            };
+
+
+            if (sampleTab === slugify(s.name)) {
+                return {
+                    ...ret,
+                    'class': 'tab-pane fade active show'
+                };
+            }
+            else {
+                return {
+                    ...ret,
+                    'class': 'tab-pane fade'
+                };
+            }
+        });
+    }
+
+    @action
+    setEventShown(element) {
         // Note: Ember doesn't want you to use JQuery for some purity reason,
         //       and it throws warnings when the app starts.
         //       Unfortunately, JQuery is the only way to add an event listener
         //       to a bootstrap tab.
-        $(this.element).find('a[data-toggle="tab"][tab-level="0"]')
-                       .off("shown.bs.tab")
-                       .on('shown.bs.tab', this, this.actions.shown);
-    },
-
-    actions: {
-        shown(event) {
-            event.data.updateSampleTab(event.currentTarget.hash);
-        },
-
-        updateShortSampleName(idx) {
-            this.oil.samples[idx].short_name = event.target.value;
-            this.submit(this.oil);
-        }
+        $(element).off("shown.bs.tab").on('shown.bs.tab', this, this.shown);
     }
 
-});
+    @action
+    shown(event) {
+        event.data.args.updateSampleTab(event.currentTarget.hash);
+    }
+
+    @action
+    updateShortSampleName(idx) {
+        this.args.oil.samples[idx].short_name = event.target.value;
+        this.args.submit(this.args.oil);
+    }
+
+}
--- web_client/app/controllers/oils/show.js
+++ web_client/app/controllers/oils/show.js
@@ -1,6 +1,18 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from "@ember/object";
 
 export default class ShowController extends Controller {
-    currentSampleTab = "#nav-fresh-oil-sample"
-    currentCategoryTab = '#nav-physical-fresh-oil-sample'
+    @tracked currentSampleTab = "#nav-fresh-oil-sample";
+    @tracked currentCategoryTab = '#nav-physical-fresh-oil-sample';
+
+    @action
+    updateSampleTab(newTab) {
+        this.currentSampleTab = newTab;
+    }
+
+    @action
+    updateCategoryTab(newTab) {
+        this.currentCategoryTab = newTab;
+    }
 }
