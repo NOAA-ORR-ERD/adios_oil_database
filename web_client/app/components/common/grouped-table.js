7a10815876948d2d885bf63e2e5c6108d9855186
James L. Makela <james.makela@noaa.gov>
2020-08-31 11:28:38 -0700

    web client: some jslint fixes

--- web_client/app/components/common/grouped-table.js
+++ web_client/app/components/common/grouped-table.js
@@ -1,6 +1,6 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import { action, set } from "@ember/object";
+import { action } from "@ember/object";
 
 export default class GroupedTable extends Component {
     @tracked baseProperty = [];
--- web_client/app/components/dialog/select-unit.js
+++ web_client/app/components/dialog/select-unit.js
@@ -1,6 +1,6 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import { action, set } from "@ember/object";
+import { action } from "@ember/object";
 import $ from 'jquery';
 
 import Nucos from 'nucos/nucos';
