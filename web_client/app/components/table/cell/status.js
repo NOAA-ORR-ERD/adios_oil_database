4fed1734568df3f64154983e8ef8ddec0646248d
James L. Makela <james.makela@noaa.gov>
2020-10-20 14:30:29 -0700

    web_client: status icons now show records with warnings and errors.

--- web_client/app/components/table/cell/status.js
+++ web_client/app/components/table/cell/status.js
@@ -1,4 +1,30 @@
 import Component from '@glimmer/component';
 
 export default class Status extends Component {
+    get hasErrors() {
+        let value = this.args.value;
+
+        if (value && value.length > 0) {
+            return value.map(i => {
+                return i.substring(0, 1) == 'E'
+            }).reduce((sum, i) => sum || i, false);
+        }
+        else {
+            return false;
+        }
+    }
+
+    get hasWarnings() {
+        let value = this.args.value;
+
+        if (value && value.length > 0) {
+            return value.map(i => {
+                return i.substring(0, 1) == 'W'
+            }).reduce((sum, i) => sum || i, false);
+        }
+        else {
+            return false;
+        }
+    }
+
 }
