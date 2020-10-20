c02fffd89eacf97109a5a0f361ef7fac538fb6b6
Chris Barker <Chris.Barker@noaa.gov>
2020-10-19 17:54:45 -0700

    started fix_API script -- but loading an Oil object from JSON is broken.

--- oil_db/oil_database/scripts/add_API.py
+++ oil_db/oil_database/scripts/add_API.py
@@ -7,21 +7,42 @@ script to go through all the data, and add an API if one is not already there
 import sys
 from pathlib import Path
 
-def get_all_records(dir):
+from oil_database.models.oil.oil import Oil
+import json
+
+class Fake():
+    oil_id = "XXXXXXXXX"
+
+
+def get_all_records(data_dir):
     """
     a generator that gets all the records, returning them one by one
     as record, path pairs
 
-    :param dir: the directory that holds the data
+    :param data_dir: the directory that holds the data
     """
-    dir = Path(dir)
-    for fname in dir.rglob():
-        yield fname
+    dir = Path(data_dir)
+    for fname in dir.rglob("*.json"):
+        with open(fname, encoding='utf-8') as jfile:
+            pyjson = json.load(jfile)
+        rec = Oil(pyjson)
+        yield rec, fname
 
 
 if __name__ == "__main__":
-    base_dir = sys.argv[1]
+    try:
+        base_dir = sys.argv[1]
+    except IndexError:
+        print("you must pass in the base dir where the data are stored")
+        sys.exit(1)
 
-    for rec, pth in get_all_records(dir):
+    for rec, pth in get_all_records(base_dir):
+        print("\n\n******************\n")
+        print(type(rec))
+        print(type(rec.oil_id))
         print("processing:", rec.oil_id)
+        print("API is:", rec.metadata.API)
+        break
+
+
 
