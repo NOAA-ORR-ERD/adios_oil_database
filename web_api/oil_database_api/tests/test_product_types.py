f95d27d08ff26b5823030f93a4a15143ff969862
Chris Barker <Chris.Barker@noaa.gov>
2020-11-12 18:19:46 -0800

    fixed import

--- web_api/oil_database_api/tests/test_product_types.py
+++ web_api/oil_database_api/tests/test_product_types.py
@@ -3,10 +3,10 @@ Functional tests for the product_types API
 
 """
 
-import pytest
-from webtest.app import AppError
+# import pytest
+# from webtest.app import AppError
 
-from oil_database.models.oil.product_types import PRODUCT_TYPES
+from oil_database.models.oil.product_type import PRODUCT_TYPES
 
 # NOTE: testapp coming from conftest.py
 
