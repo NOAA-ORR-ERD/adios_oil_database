b41b23a2c2e81c09ac14ea440e4a8c7ebe5b67bc
Chris Barker <Chris.Barker@noaa.gov>
2020-11-18 18:28:29 -0800

    added code to read product types and labels from a CSV file

--- oil_db/oil_database/models/oil/product_type.py
+++ oil_db/oil_database/models/oil/product_type.py
@@ -6,28 +6,64 @@ This maps to the JSON used in the DB
 
 Having a Python class makes it easier to write importing, validating etc, code.
 """
+from pathlib import Path
+import csv
 
 from ..common.validators import EnumValidator
 from .validation.warnings import WARNINGS
 
+# this is now loaded from a file -- see end of this module
 
-PRODUCT_TYPES = ('Crude Oil, NOS',
-                 'Condensate',
-                 'Bitumen Blend',
-                 'Refined Product, NOS',
-                 'Fuel Oil, NOS',
-                 'Distillate Fuel Oil',
-                 'Petroleum-Derived Solvent',
-                 'Residual Fuel Oil',
-                 'Bio-Petroleum Fuel Blend',
-                 'Bio Fuel Oil',
-                 'Lube Oil',
-                 'Refinery Intermediate',
-                 'Natural Plant Oil',
-                 'Dielectric Oil',
-                 'Other'
-                 )
+# PRODUCT_TYPES = ('Crude Oil, NOS',
+#                  'Condensate',
+#                  'Bitumen Blend',
+#                  'Refined Product, NOS',
+#                  'Fuel Oil, NOS',
+#                  'Distillate Fuel Oil',
+#                  'Petroleum-Derived Solvent',
+#                  'Residual Fuel Oil',
+#                  'Bio-Petroleum Fuel Blend',
+#                  'Bio Fuel Oil',
+#                  'Lube Oil',
+#                  'Refinery Intermediate',
+#                  'Natural Plant Oil',
+#                  'Dielectric Oil',
+#                  'Other'
+#                  )
 
+def load_from_csv_file(filepath=None):
+    """
+    Loads the product types and labels mapping from a CSV file
+
+    :param filepath=None: The name of the file to load from. If not
+                          provided, it will look for:
+                          "product_types_and_labels.csv" next to this module
+
+    """
+
+    if filepath is None:
+        filepath = Path(__file__).parent / "product_types_and_labels.csv"
+
+    with open(filepath, newline='') as csvfile:
+        # skip the header:
+        while True:
+            line = csvfile.readline()
+            if line.strip().startswith("DATA TABLE:"):
+                break
+
+        ptypes_labels = {}
+        reader = csv.reader(csvfile, dialect='excel')
+        labels = next(reader)[2:]
+        for row in reader:
+            pt = row[0]
+            for i, val in enumerate(row[2:]):
+                if val.strip():
+                    ptypes_labels.setdefault(pt, set()).add(labels[i])
+        return ptypes_labels
+
+
+PRODUCT_TYPE_LABEL_MAPPING = load_from_csv_file()
+PRODUCT_TYPES = tuple(PRODUCT_TYPE_LABEL_MAPPING)
 
 class ProductType(str):
     _valid_types = PRODUCT_TYPES
@@ -40,3 +76,10 @@ class ProductType(str):
         if not value:
             return [WARNINGS["W002"]]
         return cls._validator(value)
+
+
+
+
+
+
+
--- oil_db/oil_database/test/test_models/test_oil/test_product_type.py
+++ oil_db/oil_database/test/test_models/test_oil/test_product_type.py
@@ -1,8 +1,15 @@
+"""
+tests for the product type model
+"""
+
+
+from pathlib import Path
 
 import pytest
 
 from oil_database.models.oil.product_type import (ProductType,
                                                   PRODUCT_TYPES,
+                                                  load_from_csv_file,
                                                   )
 
 product_types_lower = [pt.lower() for pt in PRODUCT_TYPES]
@@ -28,4 +35,26 @@ def test_ProductType_validation_invalid(product_type):
     result = pt.validate(pt)
     assert len(result) == 1
     assert result[0].startswith("W003:")
+
+
+def test_load_from_csv_file():
+    """
+    tests loading the product types and labels from a CSV file
+    """
+    example_file = Path(__file__).parent / "example_products.csv"
+
+    mapping = load_from_csv_file(example_file)
+
+    assert list(mapping.keys()) == ['Crude Oil NOS', 'Condensate', 'Bitumen Blend']
+    assert mapping['Crude Oil NOS'] == {'Light Crude',
+                                        'Tight Oil',
+                                        'Crude Oil',
+                                        'Medium Crude',
+                                        'Group V',
+                                        'Heavy Crude',
+                                        'Bitumen Blend'}
+
+
+
+
 
