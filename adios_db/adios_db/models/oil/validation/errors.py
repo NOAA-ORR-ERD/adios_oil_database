f2f7dd7e14752f0608fa93fa130b8e46ced66c53
Chris Barker <Chris.Barker@noaa.gov>
2021-01-19 20:34:56 -0800

    fixed error msg on API

--- adios_db/adios_db/models/oil/validation/errors.py
+++ adios_db/adios_db/models/oil/validation/errors.py
@@ -1,7 +1,7 @@
 
 
 ERRORS = {"E001": "Record has no oil_id: every record must have an ID",
-          "E002": "Crude Oils must have an API",
+          "E002": "Oils must have an API",
           "E003": "No Properties data at all",
           "E004": "Reference year: {} is not a valid year (between {} and {})"
           }
