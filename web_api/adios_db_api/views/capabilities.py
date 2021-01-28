cffd581ab0686bee09f2b8e7c83d798d82fd996a
James L. Makela <james.makela@noaa.gov>
2021-01-27 19:34:02 -0800

    web_api: capabilities view needs to be compatible with ember client models.

--- web_api/adios_db_api/views/capabilities.py
+++ web_api/adios_db_api/views/capabilities.py
@@ -1,9 +1,11 @@
 from cornice import Service
+from pyramid.httpexceptions import (HTTPBadRequest,
+                                    HTTPNotFound)
 
-from ..common.views import cors_policy
+from adios_db_api.common.views import cors_policy, obj_id_from_url
 
 
-capabilities_api = Service(name='capabilities', path='/capabilities',
+capabilities_api = Service(name='capabilities', path='/capabilities*obj_id',
                            description=('List the capabilities of the '
                                         'oil database API'),
                            cors_policy=cors_policy)
@@ -15,7 +17,22 @@ def get_capabilities(request):
         List the capabilities of the oil database API
     '''
     prefix = 'caps.'
+    caps = dict([(s[len(prefix):], request.registry.settings[s])
+                 for s in request.registry.settings
+                 if s.startswith(prefix)])
+    caps.update({'_id': 0})
 
-    return dict([(s[len(prefix):], request.registry.settings[s])
-                for s in request.registry.settings
-                if s.startswith(prefix)])
+    obj_id = obj_id_from_url(request)
+
+    if obj_id is not None:
+        try:
+            obj_id = int(obj_id)
+        except TypeError as e:
+            raise HTTPBadRequest(e)
+
+        if obj_id != 0:
+            raise HTTPNotFound()
+
+        return caps
+    else:
+        return [caps]
