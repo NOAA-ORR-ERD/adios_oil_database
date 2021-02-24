8dcba057e821353ea86de12c0a2f88173386b76b
James L. Makela <james.makela@noaa.gov>
2021-02-24 09:22:51 -0800

    web_client: survey should be template-only

--- web_client/app/components/dialog/survey.js
+++ web_client/app/components/dialog/survey.js
@@ -1,25 +1,3 @@
-import Component from '@glimmer/component';
-import { action } from "@ember/object";
+import templateOnly from '@ember/component/template-only';
 
-
-export default class SurveyDlg extends Component {
-    @action
-    setModalEvents(element) {
-        // Note: Bootstrap modals can not be modified with event handlers until
-        //       the elements are completely rendered.  So we can't add an 'on'
-        //       handler in the template like you would normally be able to do.
-        //       So we add a did-insert handler in the template, attaching it
-        //       to this function.
-        // Note: Ember doesn't want you to use JQuery for some purity reason,
-        //       and it throws warnings when the app starts.
-        //       Unfortunately, JQuery is the only way to add an event listener
-        //       to a bootstrap modal.
-        //       Don't believe me?  https://stackoverflow.com/questions/24211185/twitter-bootstrap-why-do-modal-events-work-in-jquery-but-not-in-pure-js
-        //$(element).on('shown.bs.modal', this.shown);  // eslint-disable-line ember/no-jquery
-    }
-
-    //@action
-    //shown(event) {
-    //    event.currentTarget.querySelector('input').focus();
-    //}
-}
+export default templateOnly();
