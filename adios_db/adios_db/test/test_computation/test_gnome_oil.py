ecae7bf7e63e1ff608aba40f5a1821fb59c28f0a
Caitlin O'Connor <caitlin.oconnor@noaa.gov>
2021-04-02 13:01:45 -0700

    tests for kvis and SARA

--- adios_db/adios_db/test/test_computation/test_gnome_oil.py
+++ adios_db/adios_db/test/test_computation/test_gnome_oil.py
@@ -9,7 +9,7 @@ import pytest
 pytest.importorskip("scipy.optimize")
 
 from adios_db.models.oil.oil import Oil
-from adios_db.computation.gnome_oil import make_gnome_oil
+from adios_db.computation.gnome_oil import make_gnome_oil, sara_totals
 
 ExampleRecordFile = Path(__file__).parent.parent / "test_models" / "test_oil" / "ExampleFullRecord.json"
 
@@ -44,6 +44,23 @@ def test_densities():
     assert list(data['densities']) == [939.88, 925.26]
     assert list(data['density_ref_temps']) == [273.15, 288.15]
     assert list(data['density_weathering']) == [0.0, 0.0]
+
+
+def test_kvis():
+
+    data = make_gnome_oil(FullOil)
+
+    assert list(data['kvis']) == [0.0013831552964208198, 0.0003782720532607051]
+    assert list(data['kvis_ref_temps']) == [273.15, 288.15]
+    assert list(data['kvis_weathering']) == [0.0, 0.0]
+
+
+def test_SARA():
+
+    #data = make_gnome_oil(FullOil)
+    saturates, aromatics, resins, asphaltenes = sara_totals(FullOil)
+
+    assert [saturates, aromatics, resins, asphaltenes] == [0.38, 0.31, .16, .15]
 
 
 
