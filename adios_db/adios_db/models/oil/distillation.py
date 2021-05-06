0ed86ab6697572c6213d45c2626bbf62b0768eb1
James L. Makela <james.makela@noaa.gov>
2021-05-06 16:54:17 -0700

    web_client: Added distillation type, fraction recovered, & distillation method to distillation tab pane

--- adios_db/adios_db/models/oil/distillation.py
+++ adios_db/adios_db/models/oil/distillation.py
@@ -20,6 +20,7 @@ from ..common.validators import EnumValidator
 from .validation.warnings import WARNINGS
 from .validation.errors import ERRORS
 
+
 @dataclass_to_json
 @dataclass
 class DistCut:
@@ -29,7 +30,6 @@ class DistCut:
 
 class DistCutList(JSON_List):
     item_type = DistCut
-
 
 
 @dataclass_to_json
@@ -43,22 +43,26 @@ class Distillation:
 
     def validate(self):
         msgs = []
-        if self.cuts: # only need to validate if there are cuts
+        if self.cuts:  # only need to validate if there are cuts
             msgs.extend(EnumValidator({"mass fraction", "volume fraction"},
                                       ERRORS["E032"],
                                       case_insensitive=True)(self.type))
         if self.fraction_recovered is not None:
             frac_recov = self.fraction_recovered.converted_to("fraction").value
             if not (0.0 <= frac_recov <= 1.0):
-                msgs.append(ERRORS["E041"].format("distillation fraction recovered", frac_recov))
+                msgs.append(ERRORS["E041"]
+                            .format("distillation fraction recovered",
+                                    frac_recov))
 
         for cut in self.cuts:
             frac = cut.fraction.converted_to('fraction').value
             if not (0.0 <= frac <= 1.0):
-                msgs.append(ERRORS["E041"].format("distillation fraction", frac))
+                msgs.append(ERRORS["E041"]
+                            .format("distillation fraction", frac))
             vt = cut.vapor_temp.convert_to('C').value
             if vt < -100.0:
                 t = f"{cut.vapor_temp.value:.2f} {cut.vapor_temp.unit}"
-                msgs.append(ERRORS["E040"].format("distillation vapor temp", t))
+                msgs.append(ERRORS["E040"]
+                            .format("distillation vapor temp", t))
 
         return msgs
--- web_client/app/components/tab-pane/distillation.js
+++ web_client/app/components/tab-pane/distillation.js
@@ -1,3 +1,53 @@
-import templateOnly from '@ember/component/template-only';
+import Component from '@glimmer/component';
+import { action, set } from "@ember/object";
+import { isBlank } from '@ember/utils';
 
-export default templateOnly();
+export default class OilDemographics extends Component {
+    constructor() {
+        super(...arguments);
+    }
+
+    @action
+    updateDistillationType(event) {
+        let enteredType = event.target.value;
+
+        if (isBlank(enteredType)) {
+            delete this.args.oil.distillation_data.type;
+        }
+        else {
+            set(this.args.oil.distillation_data, 'type', enteredType);
+        }
+
+        this.args.submit(this.args.oil);
+    }
+
+    @action
+    updateDistillationMethod(event) {
+        let enteredMethod = event.target.value;
+
+        if (isBlank(enteredMethod)) {
+            delete this.args.oil.distillation_data.method;
+        }
+        else {
+            set(this.args.oil.distillation_data, 'method', enteredMethod);
+        }
+
+        this.args.submit(this.args.oil);
+    }
+
+    @action
+    updateFractionRecovered(event) {
+        let enteredFraction = event.target.value;
+
+        if (isBlank(enteredFraction)) {
+            delete this.args.oil.distillation_data.fraction_recovered;
+        }
+        else {
+            set(this.args.oil.distillation_data, 'fraction_recovered',
+                enteredFraction);
+        }
+
+        this.args.submit(this.args.oil);
+    }
+
+}
