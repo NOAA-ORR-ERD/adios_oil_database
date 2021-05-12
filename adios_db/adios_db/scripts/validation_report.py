f8dab34c228b9b69b9d0eb9b75e57a9753d85920
Chris Barker <Chris.Barker@noaa.gov>
2021-05-12 12:28:58 -0700

    updated validation report to write rst

--- adios_db/adios_db/scripts/validation_report.py
+++ adios_db/adios_db/scripts/validation_report.py
@@ -7,13 +7,16 @@ generates a validation report from a collection of oil JSON files
 import sys
 import datetime
 import json
+from pathlib import Path
 
 from adios_db.scripting import get_all_records
 
 USAGE = """
 adios_validate data_dir [save]
 
-Generate a validation report for the JSON files in data_dir
+Validation reports for the JSON files in data_dir
+
+These reports are in RST format.
 
 If "save" is on the command line, the. status will be updated
 with the latest validation.
@@ -28,7 +31,7 @@ def main():
         save = False
 
     try:
-        base_dir = sys.argv[1]
+        base_dir = Path(sys.argv[1])
     except IndexError:
         print(USAGE)
         sys.exit(1)
@@ -38,11 +41,12 @@ def main():
     with (open("validation_by_record.txt", 'w', encoding="utf-8") as outfile1,
           open("validation_by_error.txt", 'w', encoding="utf-8") as outfile2):
         for of in (outfile1, outfile2):
-            of.write("Validation of data in: \n")
-            of.write(base_dir)
-            of.write("\n")
-            of.write(str(datetime.datetime.now()))
-            of.write("\n\n")
+            of.write("\n####################################\n")
+            of.write("ADIOS Oil Database Validation Report\n")
+            of.write("####################################\n\n")
+            of.write("Validation of data in: \n\n")
+            of.write(f"``{base_dir.absolute()}``\n\n")
+            of.write(f"**Generated:** {datetime.datetime.now()}\n\n")
         for oil, pth in get_all_records(base_dir):
             print("\n\n******************\n")
             print(f"processing: {oil.oil_id}: {oil.metadata.name}")
@@ -50,17 +54,18 @@ def main():
             oil.reset_validation()
             print(oil.status)
             if oil.status:
-                outfile1.write(f"{oil.oil_id}: {oil.metadata.name}\n")
+                outfile1.write(f"\n``{oil.oil_id}``: {oil.metadata.name}\n")
                 for msg in oil.status:
                     validation.setdefault(msg.split(":")[0], []).append(
-                                          f"{oil.oil_id} -- {oil.metadata.name}:\n    {msg}\n")
-                    outfile1.write(f"    {msg}\n")
+                                          f"\n``{oil.oil_id}`` -- {oil.metadata.name}:\n\n    {msg}\n")
+                    outfile1.write(f" |    {msg}\n")
             if save:
                 with open(pth, 'w', encoding='utf-8') as datafile:
                         json.dump(oil.py_json(), datafile, indent=4)
         # write out the report by Error Code:
         for code, errors in validation.items():
-            outfile2.write(f"\n\n{code}:\n")
+
+            outfile2.write(f"\n\n{code}:\n=====\n")
             outfile2.writelines(errors)
 
 
