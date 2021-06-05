08cd9952171590a23203071e85db16e3184f15d9
James L. Makela <james.makela@noaa.gov>
2021-06-04 18:34:11 -0700

    web_client: Getting an exception when sample.metadata.fraction_evaporated is not there.

--- web_client/app/components/tab-pane/subsample-metadata.js
+++ web_client/app/components/tab-pane/subsample-metadata.js
@@ -5,7 +5,8 @@ import { capitalize } from '@ember/string';
 
 export default class SubsampleMetadata extends Component {
     get fractionEvaporatedUnitType() {
-        let unitType = this.args.oil.metadata.fraction_evaporated.unit_type || '';
+        let unitType = ((this.args.oil.metadata||{}).fraction_evaporated||{}).unit_type || '';
+
         return capitalize(unitType.substring(0, unitType.length - 'fraction'.length));
     }
 
