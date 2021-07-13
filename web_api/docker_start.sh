908e6316337a973bb560fd28d1ccbf6cb93ba164
James L. Makela <james.makela@noaa.gov>
2021-07-13 14:08:20 -0700

    CI: initiate saving files to noaa-oil-data when deploying web_api_stage

--- web_api/docker_start.sh
+++ web_api/docker_start.sh
@@ -3,12 +3,16 @@
 if $MONGODB_WRITEABLE
 then 
     echo "MongoDB is writeable"
+    cd /adios-db/noaa-oil-data/
+    adios_db_backup --config /config/config_oil_db.ini
+    git status
+    cd -
 else
     echo "MongoDB is read-only"
 
     echo "Loading the database from noaa-oil-data"
     cd /adios-db/noaa-oil-data/
-    adios_db_restore --config /config/config_oil_db.ini 
+    adios_db_restore --config /config/config_oil_db.ini
     cd -
 fi
 
