f0f186e60843e00de4358303e0cbd18c2acd8ac1
James L. Makela <james.makela@noaa.gov>
2021-09-30 17:52:12 -0700

    CI: Typo in docker start script, got rid of some commented code.

--- web_api/docker_start.sh
+++ web_api/docker_start.sh
@@ -41,7 +41,7 @@ if [ “$REFRESH_INTERNAL_DB” == “true” ]; then
 elif [ “$MONGODB_WRITEABLE” == “true” ]; then
     echo "Oil Database is writeable"
 
-    cd ./$REPO_NAME/
+    cd /$REPO_NAME/
 
     # getting the branches we need in
     echo ">>> git remote set-branches --add origin under_review"
@@ -112,62 +112,6 @@ elif [ “$MONGODB_WRITEABLE” == “true” ]; then
     # DONE -- the server is now running
 
     cd -
-
-# # previous setup -- delete when the new one is working
-#     echo "Oil Database is writeable"
-#     cd /noaa-oil-data/
-
-#     git config --global user.email "adios-script@noaa.gov"
-#     git config --global user.name "Adios Automated Script"
-
-#     # make sure both of our repo branches are current
-#     echo ">>> checkout & pull production branch"
-#     git checkout production
-#     git pull
-
-#     echo ">>> checkout & pull under_review branch"
-
-#     echo ">>> git remote set-branches --add origin under_review"
-#     git remote set-branches --add origin under_review
-#     echo "return code: $?"$'\n'
-
-#     echo ">>> git fetch"
-#     git fetch
-#     echo "return code: $?"$'\n'
-
-#     echo ">>> git checkout --track origin/under_review"
-#     git checkout --track origin/under_review
-#     echo "return code: $?"$'\n'
-
-#     echo ">>> git pull"
-#     git pull
-#     echo "return code: $?"$'\n'
-
-#     echo ">>> Backing up the database to noaa-oil-data"
-#     adios_db_backup --config /config/config_oil_db.ini
-
-#     echo ">>> Stage our changes"
-#     git add --all
-
-#     echo ">>> Perform a commit only if there are changes"
-#     git diff-index --quiet HEAD || git commit -m "Archiving changes to under_review from pipeline"
-
-#     echo ">>> Git status"
-#     git status
-
-#     echo ">>> pushing under_review branch"
-#     git push origin under_review
-
-#     # This should incorporate any changes in the production branch that have
-#     # been made outside the scope of the under_review branch, so they will be
-#     # seen as changes in the under_review branch history.
-#     echo ">>> rebase production under_review"
-#     git rebase production under_review
-
-#     echo ">>> Refresh the database with new baseline"
-#     adios_db_restore --config /config/config_oil_db.ini
-
-#     cd -
 else
     echo "MongoDB is read-only"
 
