233561173816198963b06a01f3f2f6243d6bb2a9
James L. Makela <james.makela@noaa.gov>
2021-10-01 22:56:30 -0700

    web_client: distillation table having problems editing a new oil record.

--- web_client/app/components/table/distillation.js
+++ web_client/app/components/table/distillation.js
@@ -4,7 +4,7 @@ import { action, set } from "@ember/object";
 export default class Distillation extends Component {
     @action
     submit(cutsValue) {
-        set(this.args.oil, 'cuts', cutsValue);
+        set(this.args.oil.distillation_data, 'cuts', cutsValue);
         this.args.submit(this.args.oil);
     }
 }
