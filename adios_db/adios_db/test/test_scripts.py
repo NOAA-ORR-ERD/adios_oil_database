2c12cd5ec693b9a75be7229f68a456e3951eb676
Chris Barker <Chris.Barker@noaa.gov>
2021-12-23 15:00:43 -0800

    a start on a test for the backup script

---
+++ adios_db/adios_db/test/test_scripts.py
@@ -0,0 +1,31 @@
+"""
+test the various scripts
+
+These don't test whether they work right, but should at least check that they
+will run without errors
+"""
+
+import pytest
+
+from adios_db.scripts import db_backup
+
+
+
+# Pass the --mongo command line option if you want these to run.
+# they require a mongo database to be running on localhost
+pytestmark = pytest.mark.skipif(True, reason="not working yet" )
+
+# pytestmark = pytest.mark.mongo
+
+# OUTPUT_DIR = here / "output_data"
+
+
+def test_backup_script():
+    settings = default_settings()
+    base_path = ""
+    db_backup.backup_db(settings, base_path)
+
+    assert False
+
+
+
