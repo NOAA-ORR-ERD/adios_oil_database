3d723de1c51a5799e1d72e7a91fc0c680f04064e
Chris Barker <Chris.Barker@noaa.gov>
2023-03-16 17:18:24 -0700

    reading metadata, and start on physical properties

--- adios_db/adios_db/scripts/import_csv.py
+++ adios_db/adios_db/scripts/import_csv.py
@@ -15,9 +15,13 @@ import warnings
 
 from adios_db.models.oil.oil import Oil
 from adios_db.models.oil.version import Version
-from adios_db.models.oil.metadata import MetaData
+from adios_db.models.oil.metadata import MetaData, SampleMetaData
+from adios_db.models.oil.sample import Sample
 from adios_db.models.oil.product_type import ProductType
 from adios_db.models.oil.values import Reference
+from adios_db.models.oil.physical_properties import PhysicalProperties
+from adios_db.models.common.measurement import MassFraction, Temperature
+
 
 def read_csv_file(infilename):
     """
@@ -45,6 +49,10 @@ def read_csv_file(infilename):
             break
     # read the metadata:
     oil.metadata = read_record_metadata(reader)
+    # load the subsamples:
+    while True:
+        oil.sub_samples.append(read_subsample(reader))
+        break
 
     return oil
 
@@ -59,24 +67,21 @@ def read_record_metadata(reader):
     # location_coordinates: LocationCoordinates = None
 
     metadata_map = {
-        normalize("Name"): ("name", str),
-        normalize("Source ID"): ("source_id", str),
-        normalize("Location"): ("location", str),
-        normalize("Sample Date"): ("sample_date", str),
-        normalize("Comments"): ("comments", str),
+        normalize("Name"): ("name", strstrip),
+        normalize("Source ID"): ("source_id", strstrip),
+        normalize("Location"): ("location", strstrip),
+        normalize("Sample Date"): ("sample_date", strstrip),
+        normalize("Comments"): ("comments", strstrip),
         normalize("API Gravity"): ("API", float),
         normalize("Product Type"): ("product_type", ProductType),
         # normalize("Product Type"): ("reference", Reference),
     }
-    print("reading metadata")
     md = MetaData()
     # look for subsample data, then stop
     for row in reader:
         if check_field_name(row[0], "Subsample Metadata"):
-            print("found subsample, breaking out")
             break
         else:
-            print("reading metadata")
             # this could be more efficient with a dict lookup, rather than a loop
             # but would require normalization
             try:
@@ -93,7 +98,124 @@ def read_record_metadata(reader):
 
     return md
 
+def read_subsample(reader):
+    ss = Sample(metadata=read_subsample_metadata(reader))
+    ss.physical_properties = read_physical_properties(reader)
 
+    return ss
+
+def read_subsample_metadata(reader):
+    """
+    Read the record metadata -- stops when "Physical Properties" is reached.
+
+    returns a SubsampleMetadata object
+
+    class SampleMetaData:
+    name: str = "Fresh Oil Sample"
+    short_name: str = None
+    sample_id: str = None
+    description: str = None
+    fraction_evaporated: MassFraction = None
+    boiling_point_range: Temperature = None
+    """
+
+    # reference: Reference = field(default_factory=Reference)
+    # location_coordinates: LocationCoordinates = None
+
+    metadata_map = {
+        normalize("Name"): ("name", strstrip),
+        normalize("Short name"): ("short_name", strstrip),
+        normalize("Sample ID"): ("sample_id", strstrip),
+        normalize("Description"): ("description", strstrip),
+    }
+    md = SampleMetaData()
+    # look for Physical Properties data, then stop
+    for row in reader:
+        print("Processing:", row)
+        if check_field_name(row[0], "Physical Properties"):
+            # print("found Physical Properties: breaking out")
+            break
+        else:
+            # this could be more efficient with a dict lookup, rather than a loop
+            # but would require normalization
+            try:
+                if row[1]:
+                    attr, func = metadata_map[normalize(row[0])]
+                    setattr(md, attr, func(row[1]))
+            except KeyError:
+                if check_field_name(row[0], "Fraction evaporated"):
+                    md.fraction_evaporated = MassFraction(value=float(row[2]), unit=strstrip(row[3]))
+                if check_field_name(row[0], "Boiling Point Range"):
+                    md.boiling_point_range = Temperature(min_value=float(row[1]),
+                                                         max_value=float(row[2]),
+                                                         unit=strstrip(row[3]))
+    return md
+
+def read_physical_properties(reader):
+    """
+    read the physical properties data
+
+    Stops when "Distillation Data" is reached
+
+    class PhysicalProperties:
+
+    pour_point: PourPoint = None
+    flash_point: FlashPoint = None
+
+    densities: DensityList = field(default_factory=DensityList)
+    kinematic_viscosities: KinematicViscosityList = field(default_factory=KinematicViscosityList)
+    dynamic_viscosities: DynamicViscosityList = field(default_factory=DynamicViscosityList)
+
+    interfacial_tension_air: InterfacialTensionList = field(default_factory=InterfacialTensionList)
+    interfacial_tension_water: InterfacialTensionList = field(default_factory=InterfacialTensionList)
+    interfacial_tension_seawater: InterfacialTensionList = field(default_factory=InterfacialTensionList)
+    """
+    pp = PhysicalProperties()
+
+    # pp_map = {
+    #     normalize("Name"): ("name", strstrip),
+    #     normalize("Short name"): ("short_name", strstrip),
+    #     normalize("Sample ID"): ("sample_id", strstrip),
+    #     normalize("Description"): ("description", strstrip),
+    # }
+    # look for "Distillation Data" data, then stop
+    for row in reader:
+        print("Processing:", row)
+        if check_field_name(row[0], "Distillation Data"):
+            print('found "Distillation Data": breaking out')
+            break
+        else:
+            pass
+            # # this could be more efficient with a dict lookup, rather than a loop
+            # # but would require normalization
+            # try:
+            #     if row[1]:
+            #         attr, func = metadata_map[normalize(row[0])]
+            #         setattr(md, attr, func(row[1]))
+            # except KeyError:
+
+            if check_field_name(row[0], "Pour Point"):
+                pp.pour_point = Temperature(**read_measurement(row[1:]))
+            if check_field_name(row[0], "Flash Point"):
+                pp.flash_point = Temperature(**read_measurement(row[1:]))
+
+    return pp
+
+def read_measurement(items):
+    """
+    reads a sequence, and returns a dict of measurement values:
+
+    min_value=float(row[1]),
+    value=float(row[2]),
+    max_value=float(row[3]),
+    unit
+    """
+    vals = {}
+    for key, val in zip(('min_value', 'value', 'max_value' ), items[:3]):
+       vals[key] = float(val) if val.strip() else None
+    vals['unit'] = strstrip(items[3])
+
+    return vals
 
 def normalize(name):
     """
@@ -129,6 +251,8 @@ def main():
     print(f"Saving: {outfilename} as JSON")
     oil.to_file(outfilename)
 
+def strstrip(obj):
+    return str(obj).strip()
 
 if __name__ == "__main__":
     main()
--- adios_db/adios_db/test/test_importing/test_import_from_csv.py
+++ adios_db/adios_db/test/test_importing/test_import_from_csv.py
@@ -1,38 +1,55 @@
 """
 Tests the importing from a "NOAA Standard" CSV file script.
 
-Note: tests aren't comprehensive, but at least there's something
-    if it breaks on future files - please add a test!
+Note: tests aren't comprehensive, but at least there's something.
 
+If it breaks on future files - please add a test!
 """
+
 from pathlib import Path
 
 from adios_db.models.oil.oil import ADIOS_DATA_MODEL_VERSION
+from adios_db.models.oil.metadata import MetaData
+from adios_db.models.common.measurement import MassFraction, Temperature
 
-from adios_db.scripts.import_csv import read_csv_file
+from adios_db.scripts.import_csv import read_csv_file, read_measurement
+
+import pytest
 
 test_file = Path(__file__).parent / "example_data" / "LSU_AlaskaNorthSlope.csv"
 
-def test_read_csv():
+# make module level?
+@pytest.fixture
+def test_record():
     """
-    one big honking test for the whole thing -- not ideal, but something
+    read the example record and return the resulting oil record
     """
     oil = read_csv_file(test_file)
 
+    return oil
+
+def test_read_csv(test_record):
+    """
+    one big honking test for the whole thing -- not ideal, but something
+    """
+    oil = test_record
+
     assert oil.adios_data_model_version == ADIOS_DATA_MODEL_VERSION
 
-def test_metadata():
+
+def test_metadata(test_record):
     """
     not really an isolated test, but at least errors will be a bit more specific
     """
-    md = read_csv_file(test_file).metadata
+    md = test_record.metadata
     test_map = [
         ("name", "Alaska North Slope"),
         ("source_id", ""),
-        ("location", ""),
+        ("location", "Alaska, USA"),
         ("sample_date", "2009"),
         ("comments", "The data in this record may have been compiled from multiple sources and reflect samples of varying age and composition"),
-        ("product_type", "Crude Oil NOS")
+        ("product_type", "Crude Oil NOS"),
+        ("API", 32.1),
     ]
 
     for attr, value in test_map:
@@ -43,7 +60,33 @@ def test_metadata():
     assert set(md.alternate_names) == set(['ANS'])
     assert set(md.labels) == set(['Medium Crude', 'Crude Oil'])
 
+# def test_subsamples(test_record):
+#     assert len(test_record.sub_samples) == 3
 
+@pytest.mark.parametrize("attr, value",
+                         [("name", "Original fresh oil sample"),
+                          ("short_name", "Fresh"),
+                          ("sample_id", 'x1x1x1'),
+                          ("description", 'Just a little bit of text.'),
+                          ("fraction_evaporated", MassFraction(0.034, unit='fraction')),
+                          ("boiling_point_range", Temperature(min_value=150, max_value=250, unit='C')),
+                          ])
+def test_subsample_metadata_0(attr, value, test_record):
+    md = test_record.sub_samples[0].metadata
+    assert getattr(md, attr) == value
+
+@pytest.mark.parametrize("attr, value",
+                         [('pour_point', Temperature(32, unit='C')),
+                          ('flash_point', Temperature(max_value=-8, unit='C')),
+                          ])
+def test_subsample_physical_properties(attr, value, test_record):
+    pp = test_record.sub_samples[0].physical_properties
+    assert getattr(pp, attr) == value
+
+def test_read_measurement():
+    vals = read_measurement(('1.2', '', ' ', 'unit '))
+
+    assert vals == {'min_value': 1.2, 'value': None, 'max_value': None, 'unit': 'unit'}
 
 
 
