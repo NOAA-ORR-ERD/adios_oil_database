a39af5fd3c1559ac51d7545b3c7e2d2aa5b90c4b
James.Makela <james.makela@noaa.gov>
2023-10-16 15:33:46 -0700

    web_client: node 18: Report problem button & dialog now working.

--- web_client/app/components/dialog/add-new-oil.js
+++ web_client/app/components/dialog/add-new-oil.js
@@ -1,7 +1,6 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from "@ember/object";
-import $ from 'jquery';
 
 
 export default class AddNewOilDlg extends Component {
