a6ffa8effc01445354ae6c9571ced217e42b1772
James.Makela <james.makela@noaa.gov>
2023-10-16 20:41:05 -0700

    web_client: node 18: Copy Oil button & dialog working, properties table raising exception on oil detail page load.

--- web_client/app/components/dialog/copy-oil.js
+++ web_client/app/components/dialog/copy-oil.js
@@ -1,7 +1,6 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action, set } from "@ember/object";
-import $ from 'jquery';
 
 export default class CopyOilDlg extends Component {
     @tracked oilName;
@@ -21,32 +20,17 @@ export default class CopyOilDlg extends Component {
     }
 
     @action
-    setModalEvents(element) {
-        // Note: Bootstrap modals can not be modified with event handlers until
-        //       the elements are completely rendered.  So we can't add an 'on'
-        //       handler in the template like you would normally be able to do.
-        //       So we add a did-insert handler in the template, attaching it
-        //       to this function.
-        // Note: Ember doesn't want you to use JQuery for some purity reason,
-        //       and it throws warnings when the app starts.
-        //       Unfortunately, JQuery is the only way to add an event listener
-        //       to a bootstrap modal.
-        //       Don't believe me?  https://stackoverflow.com/questions/24211185/twitter-bootstrap-why-do-modal-events-work-in-jquery-but-not-in-pure-js
-        $(element).on('shown.bs.modal', this.shown);  // eslint-disable-line ember/no-jquery
-    }
-
-    @action
-    shown(event) {
-        event.currentTarget.querySelector('input').focus();
-    }
-
-    @action
     updateName(event) {
         this.oilName = event.target.value;
 
         if (['change', 'focusout'].includes(event.type) && this.formFilledOut) {
             this.okButton.focus();
         }
+    }
+
+    @action
+    closeForm() {
+        this.args.close();
     }
 
     @action
