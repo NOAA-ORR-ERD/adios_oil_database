d399890cb8733565fbf2cf5109b99051b2de4a7b
Chris Barker <Chris.Barker@noaa.gov>
2024-01-19 09:35:34 -0800

    bumped version for adios_db

--- adios_db/adios_db/__init__.py
+++ adios_db/adios_db/__init__.py
@@ -2,7 +2,7 @@ import sys
 from logging import (basicConfig, getLogger, NullHandler,
                      DEBUG, INFO, WARNING, ERROR, CRITICAL)
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 
 
 def initialize_console_log(level='debug'):
