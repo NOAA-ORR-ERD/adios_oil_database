3fd7a31d6b5574cd2cbf0451317e3bee0089e21f
Naomi Connolly <naomi.connolly@noaa.gov>
2025-06-06 16:21:38 -0700

    adjusted sync script to work with new layout

--- web_api/sync_with_under_review.sh
+++ web_api/sync_with_under_review.sh
@@ -3,13 +3,13 @@
 # Script to sync active server with under_review branch
 
 echo "Inside Script: $0"
-set -e  # exit script if any commands fail
-set -x  # echo commands to stdout
+set -e # exit script if any commands fail
+set -x # echo commands to stdout
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${0}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${0}")" &>/dev/null && pwd)
 source ${SCRIPT_DIR}/git_functions.sh
 
-cd /data/git_repos/noaa-oil-data/
+cd /noaa-oil-data/
 
 git config user.email "adios-script@noaa.gov"
 git config user.name "Adios Automated Script"
@@ -25,8 +25,7 @@ git checkout production
 git pull -s recursive -X theirs --no-edit
 
 # sync with the latest version on the server
-if [[ $(branch_exists_remotely under_review) -eq "0" ]];
-then
+if [[ $(branch_exists_remotely under_review) -eq "0" ]]; then
     echo "The remote under_review branch exists.  Check it out and pull."
     git checkout under_review
     git pull -s recursive -X theirs --no-edit
@@ -42,8 +41,7 @@ fi
 adios_db_backup --config /config/config_oil_db.ini
 git add -A
 
-if ! git diff-index --quiet HEAD --
-then
+if ! git diff-index --quiet HEAD --; then
     # changes made
     git commit -a -m "Save from Running Editable Server"
 
@@ -55,7 +53,6 @@ else
     # just in case something just changed outside of the pipeline
     git pull -s recursive -X theirs --no-edit
 fi
-
 
 # update database with latest under_review branch
 adios_db_restore --config /config/config_oil_db.ini
