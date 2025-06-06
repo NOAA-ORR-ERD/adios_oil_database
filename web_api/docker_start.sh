4803a6c9a6ff212d4712ed32e97e6603965faa90
Naomi Connolly <naomi.connolly@noaa.gov>
2025-06-05 17:34:55 -0700

    added db modification to pserve command

--- web_api/docker_start.sh
+++ web_api/docker_start.sh
@@ -6,4 +6,4 @@ ls -la
 echo "Starting our server on host:port:"
 egrep -w "host|port" /config/config.ini
 
-/opt/conda/bin/pserve /config/config.ini
+/opt/conda/bin/pserve /config/config.ini CAN_MODIFY_DB=${CAN_MODIFY_DB:-false}
