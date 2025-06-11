b265e6e6e11cecbcb86e0e3a25d343adceab85c9
Naomi Connolly <naomi.connolly@noaa.gov>
2025-06-10 17:39:09 -0700

    switch repo init to script and updated script to new format

--- web_api/init_noaa_data.sh
+++ web_api/init_noaa_data.sh
@@ -4,35 +4,28 @@
 # adios database server
 echo "In: init_noaa_data.sh script"
 
-set -e  # exit script if any commands fail
-set -x  # echo commands to stdout
+set -e # exit script if any commands fail
+set -x # echo commands to stdout
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${0}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${0}")" &>/dev/null && pwd)
 source ${SCRIPT_DIR}/git_functions.sh
 
 REPO_NAME=noaa-oil-data
 
-cd /data/git_repos
+cd /${REPO_NAME}
 
-if [ -d $REPO_NAME ]
-then
+if [ -d $REPO_NAME ]; then
     echo "$REPO_NAME exists. Dropping the repository."
     rm -rf $REPO_NAME
 fi
 
 echo "Cloning the repo"
-git clone ssh://git@gitlab.orr.noaa.gov:9933/gnome/oil_database/$REPO_NAME.git $REPO_NAME
+git clone https://adios-api:${noaa_oil_data_token}@gitlab.orr.noaa.gov/gnome/oil_database/noaa-oil-data.git /noaa-oil-data
 
 cd $REPO_NAME
 
-git config user.email "adios-script@noaa.gov"
-git config user.name "Adios Automated Script"
-
-git status
-
 # setup the initial state of the repository
-if [ $(branch_exists under_review) ]
-then
+if [ $(branch_exists under_review) ]; then
     echo "The under_review branch already exists."
     git checkout under_review
 else
@@ -41,6 +34,5 @@ else
     git checkout -b under_review
     git push --set-upstream origin
 fi
-
 
 echo "The $REPO_NAME repository has been initialized ..."
